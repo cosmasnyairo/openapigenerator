d74df5d6267eca8dc679fbdc9f2445a862792d04
Cosmas Nyairo <nyairocosmas@gmail.com>
2023-03-21 22:18:00 +0300

    generate paths

--- main.go
+++ main.go
@@ -3,25 +3,10 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 
 	"gopkg.in/yaml.v2"
 )
-
-type OpenApiSpec struct {
-	Openapiversion string `yaml:"openapi"`
-	Servers        []Servers
-	Paths          []OpenApiPath `yaml:"paths"`
-}
-
-type OpenApiPath struct {
-	Method      string `yaml:"openapi"`
-	Integration []OpenApiPathIntegration
-}
-
-type OpenApiPathIntegration struct {
-	Url        string `yaml:"openapi"`
-	Httpmethod string `yaml:"openapi"`
-}
 
 type Apispec struct {
 	Name                  string    `yaml:"name"`
@@ -43,26 +28,86 @@ type Pathspec struct {
 	Queryparameters []string `yaml:"queryparameters,omitempty"`
 }
 
-type Model[T any] struct {
-	Paths T `yaml:"paths"`
+type OpenAPI struct {
+	OpenAPI string                 `yaml:"openapi"`
+	Info    OpenApiSpecInfo        `yaml:"info"`
+	Servers []Servers              `yaml:"servers,omitempty"`
+	Paths   map[string]OpenApiPath `yaml:"paths,omitempty"`
+	// Components OpenApiSpecComponents  `yaml:"components,omitempty"`
+	// Security   []map[string][]string  `yaml:"security,omitempty"`
+	// Tags       []*Tag                      `yaml:"tags,omitempty"`
+
 }
+
+type OpenApiSpecInfo struct {
+	Title       string `yaml:"title"`
+	Version     string `yaml:"version"`
+	Description string `yaml:"description,omitempty"`
+	Contact     struct {
+		Name  string `yaml:"name,omitempty"`
+		Email string `yaml:"email,omitempty"`
+		URL   string `yaml:"url,omitempty"`
+	} `yaml:"contact,omitempty"`
+}
+
+// type OpenApiSpecComponents struct {
+// 	// securitySchemes
+// }
+
+type OpenApiPath struct {
+	Ref                  string               `yaml:"$ref,omitempty"`
+	Summary              string               `yaml:"summary,omitempty"`
+	Description          string               `yaml:"description,omitempty"`
+	OpenApiPathOperation OpenApiPathOperation `yaml:"get,omitempty,flow"`
+
+	// Parameters  []*OpenApiPathParameter `yaml:"parameters,omitempty"`
+}
+
+type OpenApiPathOperation struct {
+	Tags        []string `yaml:"tags,omitempty"`
+	Summary     string   `yaml:"summary,omitempty"`
+	Description string   `yaml:"description,omitempty"`
+	// RequestBody OpenApiPathRequestBody `yaml:"requestBody,omitempty"`
+	// Responses   OpenApiPathResponses   `yaml:"responses,omitempty"`
+	Deprecated         bool              `yaml:"deprecated,omitempty"`
+	GatewayIntegration map[string]string `yaml:"x-amazon-apigateway-integration,omitempty"`
+}
+
+// type OpenApiResource struct {
+// 	Resource    OpenApiResource
+// 	Method      string `yaml:"openapi"`
+// 	Integration []OpenApiPathIntegration
+// }
+
+// type OpenApiPathIntegration struct {
+// 	Url        string `yaml:"openapi"`
+// 	Httpmethod string `yaml:"openapi"`
+// }
 
 func main() {
 	apispec := Apispec{}
 	apispec.getApispec()
 
-	mergePaths([]Pathspec{})
-	// for _, paths := range mergedpaths {
-	// 	fmt.Println(paths)
-	// }
+	pathspec := mergePaths([]Pathspec{})
 
-	// openapi := OpenApiSpec{
-	// 	Openapiversion: "3.0.1",
-	// 	Servers:        apispec.Servers,
-	// }
-	// yaml, err := yaml.Marshal(&openapi)
-	// onError("Unable to unmarshall", err)
-	// fmt.Println(custom)
+	var pathitems []map[string]any
+
+	for _, paths := range pathspec {
+		pathitems = append(pathitems, paths.generateOpenApiPaths())
+	}
+
+	generated, _ := yaml.Marshal(pathitems)
+
+	err := os.WriteFile("generated-paths.yaml", generated, 0644)
+	onError("Unable to write", err)
+
+	// fmt.Println(string(generated))
+
+	// pathspec, _ :=yaml.Marshal(mergePaths([]Pathspec{}))
+
+	// err := os.WriteFile("generated.yaml", merged, 0644)
+	// onError("Unable to write", err)
+
 }
 
 func mergePaths(pathspec []Pathspec) []Pathspec {
@@ -85,22 +130,40 @@ func mergePaths(pathspec []Pathspec) []Pathspec {
 
 	err = yaml.Unmarshal(pathbyte, &pathspec)
 	onError("Unable to unmarshall", err)
-
-	for _, paths := range pathspec {
-		paths.convertoOpenAPi()
-	}
-
 	return pathspec
 }
 
-func (path *Pathspec) convertoOpenAPi() {
+func (path *Pathspec) generateOpenApiPaths() map[string]any {
 
-}
+	// generate operations
+	var methods []map[string]OpenApiPathOperation
 
-func onError(message string, err error) {
-	if err != nil {
-		log.Fatalf("%v %v", message, err)
+	for _, method := range path.Methods {
+
+		methodOperation := map[string]OpenApiPathOperation{
+			strings.ToLower(method): {
+				GatewayIntegration: map[string]string{
+					"uri":          path.Uri,
+					"connectionId": "$${stageVariables.vpcLink}",
+					"httpMethod":   method,
+				},
+			},
+		}
+
+		// TODO prevent conversion to a list here
+
+		methods = append(methods, methodOperation)
+
 	}
+
+	// TODO prevent conversion to a list here
+
+	pathitem := map[string]any{
+		path.Name: methods,
+	}
+
+	return pathitem
+
 }
 
 func (a *Apispec) getApispec() {
@@ -109,4 +172,11 @@ func (a *Apispec) getApispec() {
 
 	err = yaml.Unmarshal(yamlfile, &a)
 	onError("Unable to unmarshall", err)
+
+}
+
+func onError(message string, err error) {
+	if err != nil {
+		log.Fatalf("%v %v", message, err)
+	}
 }
